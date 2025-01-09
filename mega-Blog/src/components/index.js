5e0b196e64703455d192960bfa40879ba4909620
Mjahir <hathaliyamahesh2004@gmail.com>
2025-01-09 12:07:57 +0530

    Final code before debugging

--- mega-Blog/src/components/index.js
+++ mega-Blog/src/components/index.js
@@ -1,3 +1,4 @@
+import Select from "./Select"
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
@@ -6,9 +7,11 @@ import Logoutbtn from "./Header/Logoutbtn";
 import RTE from "./RTE";
 import Signup from "./Signup";
 import Login from "./Login";
-import Post from "./Post";
 import PostForm from "./post-form/PostForm";
-import PostCard from "./PostCard"
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./input";
+import Button from "./Button";
 
 export {
     Header,
@@ -19,7 +22,10 @@ export {
     RTE,
     Signup,
     Login,
-    Post,
     PostForm,
-    PostCard
+    PostCard,
+    AuthLayout,
+    Input,
+    Select,
+    Button,
 }
